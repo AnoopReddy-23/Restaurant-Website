6b6f245832080648b809f7a9a59e930df60a282e
anoop <yedullaanoopreddy22@gmail.com>
2022-06-19 17:15:21 +0530

    test version-1

--- src/Components/Admindashboard/Admindashboard.js
+++ src/Components/Admindashboard/Admindashboard.js
@@ -1,29 +1,50 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import { Nav } from "react-bootstrap";
-import { Outlet, NavLink } from "react-router-dom";
+import { useSelector } from 'react-redux';
+import { Outlet, NavLink , useNavigate} from "react-router-dom";
 
 function Admindashboard() {
+
+  //get state from store
+  let {userObj,isError,isLoading,isSuccess,errMsg}=useSelector(state=>state.user)
+  //console.log(userObj)
+
+  let navigate=useNavigate()
+
+  useEffect(()=>{
+    if(isSuccess===false){
+      navigate('/login')
+    }
+  },[])
+
   return (
     <>
-        <Nav className="justify-content-center mt-3" defaultActiveKey="/profile">
-          <Nav.Item>
-            <Nav.Link to="profile" as={NavLink}>Admin Profile</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-              <Nav.Link to="addproduct" as={NavLink}>Add Product</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-              <Nav.Link to="products" as={NavLink}>Products</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-              <Nav.Link to="orders" as={NavLink}>Orders</Nav.Link>
-          </Nav.Item>
-          </Nav>
-          {/* outlet */}
-          <div className="mt-3">
-            <Outlet />
-          </div>
-      </>
+    {isSuccess===false 
+      ? (
+            alert("Please Login!!! After then u can use the dashboard")
+      ) 
+      : ( <>
+          <Nav className="justify-content-center mt-3" defaultActiveKey="/profile">
+            <Nav.Item>
+              <Nav.Link to="profile" as={NavLink}>Admin Profile</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link to="addproduct" as={NavLink}>Add Product</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link to="products" as={NavLink}>Products</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link to="orders" as={NavLink}>Orders</Nav.Link>
+            </Nav.Item>
+            </Nav>
+            {/* outlet */}
+            <div className="mt-3">
+              <Outlet />
+            </div>
+        </> )
+    }
+   </>
   )
 }
 
--- src/Components/Userdashboard/Userdashboard.js
+++ src/Components/Userdashboard/Userdashboard.js
@@ -1,27 +1,49 @@
 import React from 'react'
+import { useEffect } from 'react';
 import { Nav } from "react-bootstrap";
-import { Outlet, NavLink } from "react-router-dom";
+import { useSelector } from 'react-redux';
+import { Outlet, NavLink, useNavigate } from "react-router-dom";
 import './Userdashboard.css'
 
 function Userdashboard() {
+
+  //get state from store
+  let {userObj,isError,isLoading,isSuccess,errMsg}=useSelector(state=>state.user)
+  //console.log(userObj)
+
+  let navigate=useNavigate()
+
+  useEffect(()=>{
+    if(isSuccess===false){
+      navigate('/login')
+    }
+  },[])
+
   return (
-    <>
-    <Nav className="justify-content-center mt-3 userdashboard" defaultActiveKey="/profile">
-      <Nav.Item>
-        <Nav.Link to="profile" as={NavLink}>User Profile</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-          <Nav.Link to="cart" as={NavLink}>Cart</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-          <Nav.Link to="products" as={NavLink}>Products</Nav.Link>
-      </Nav.Item>
-      </Nav>
-      {/* outlet */}
-      <div className="mt-3">
-        <Outlet />
-      </div>
-  </>
+   <>
+    {isSuccess===false 
+      ? (
+            alert("Please Login!!! After then u can use the dashboard")
+      ) 
+      : ( <>
+          <Nav className="justify-content-center mt-3 userdashboard" defaultActiveKey="/profile">
+            <Nav.Item>
+              <Nav.Link to="profile" as={NavLink}>User Profile</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link to="cart" as={NavLink}>Cart</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link to="products" as={NavLink}>Products</Nav.Link>
+            </Nav.Item>
+            </Nav>
+            {/* outlet */}
+            <div className="mt-3">
+              <Outlet />
+            </div>
+        </>)
+    }
+   </>
   )
 }
 
